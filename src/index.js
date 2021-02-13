560ea2b2222008966b5f985570f3c23110877dda
MankovskiPavel <mankovskipavel@gmail.com>
2021-02-13 21:44:41 +0300

    mySolution

--- src/index.js
+++ src/index.js
@@ -1,63 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  nnn=0
-  if (str.length%2==1) {return false}
- while (str!=""){
-
-    for (let i=0;i<str.length;i++)
+if (str.length%2==1) {return false}
+let len=str.length/2
+for(let a=0;a<len;a++){
+for (let i=0;i<str.length;i++){
+if (eq(str[i],str[i+1],bracketsConfig)) 
     {
-      if (isClosed(str[i],bracketsConfig)) 
-      {
-        if (i==0){return false}
-        newst=str.slice(0,i*2)
-        if (newst!=str){ 
-         str=str.slice(i*2,str.length)
-        }   else str=""
-        break   
-      }
+    str=str.slice(0,i)+str.slice(i+2,str.length)
+    
     }
-    if (checkpart(newst,bracketsConfig)==false){return false}
-
-
- }
-
-  
-  return true
-}
-
-
-function second (st,bc){
-  for (let i=0;i<bc.length;i++){
-    if (bc[i][0]==st) return bc[i][1]
   }
+
+}
+if (str=="") return true
+return false
 }
 
-function checkpart(str,bracketsConfig){
-  while (str!=""){
-    if (str[str.length-1]!=second(str[0],bracketsConfig)) {return false}
-    str=str.slice(1,str.length-1)
-  }
-  return true
-}
-
-
-function isClosed(st,bc)
+function eq(st1,st2,bc)
 {
-  
-   for (let i=0;i<bc.length;i++)  {    
-     
-    if (bc[i][1]==st) {
-      if (bc[i][0]==bc[i][1]){
-        if (nnn==0){
-          nnn=1
-          return false
-        } 
-        else 
-        {
-          nnn=0
-          return true
-        }
-      } else return true
-    }
+  for (let ii=0;ii<bc.length;ii++){
+    if((st1==bc[ii][0])&&(bc[ii][1]==st2)){return true}
   }
   return false
 }
